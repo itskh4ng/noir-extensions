ab0605b4906276c7229c406fda03c140c24e8870
itskh4ng <itskh4ng@gmail.com>
2024-10-05 14:15:45 -0700

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(function () {var a = document.createElement('script');a.src = 'https://cdn.jsdelivr.net/gh/itskh4ng/noir-extensions@main/extension.min.js';document.body.appendChild(a);}())
+javascript:(function () {var a = document.createElement('script');a.src = 'https://cdn.jsdelivr.net/gh/itskh4ng/noir-extensions@main/extensions.min.js';document.body.appendChild(a);}())
